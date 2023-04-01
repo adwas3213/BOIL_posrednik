c29edff72173bce008c1e2ec1c4b3924d36990b0
Adam Wasylewicz <adwas33@wp.pl>
2023-04-01 20:15:41 +0200

    initial commit

---
+++ src/main/java/com/example/boilposrednik/BoilPosrednikApplication.java
@@ -0,0 +1,13 @@
+package com.example.boilposrednik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilPosrednikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoilPosrednikApplication.class, args);
+    }
+
+}
